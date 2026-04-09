0dc9d29fa44e091c24dd9b2b43bddb0ff584945b
Sattarkhan37 <abdulsattarkhan973@gmail.com>
2026-04-09 16:27:04 +0530

    added signin, signup, room middleware. added logic that allow only authorized User in room and wedsocket server

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,10 +1,30 @@
 import express from "express";
-
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
+import { middleware } from "./middleware";
 const app = express();
 const port = 3006;
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
+app.post("/signup", (req, res) => {
+  //db call
+  res.json({
+    userId: 123,
+  });
+});
+app.post("/signin", (req, res) => {
+  const userId = 1;
+  const tocken = jwt.sign(
+    {
+      userId,
+    },
+    JWT_SECRET,
+  );
+  res.json({ tocken });
+});
+app.post("/room", middleware, (req, res) => {
+  res.json({
+    roomId: 123,
+  });
 });
 
 app.listen(port, () => {
