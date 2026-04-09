dcc5d359bbcb943c22280c504326b256110ff1e1
Sattarkhan37 <abdulsattarkhan973@gmail.com>
2026-04-10 02:03:32 +0530

    build DB schema using zod and common packeages

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,12 +1,19 @@
 import express from "express";
 import jwt from "jsonwebtoken";
-import { JWT_SECRET } from "./config";
+import { JWT_SECRET } from "@repo/backend-common/config";
 import { middleware } from "./middleware";
+import { CreateUserSchema } from "@repo/common/types";
 const app = express();
 const port = 3006;
 
 app.post("/signup", (req, res) => {
   //db call
+  const data = CreateUserSchema.safeParse(req.body);
+  if (!data.success) {
+    return res.json({
+      message: "Incorrect input",
+    });
+  }
   res.json({
     userId: 123,
   });
--- apps/http-backend/src/middleware.ts
+++ apps/http-backend/src/middleware.ts
@@ -1,12 +1,12 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
-import { JWT_SECRET } from "./config";
+import { JWT_SECRET } from "@repo/backend-common/config";
 
 export function middleware(req: Request, res: Response, next: NextFunction) {
   const token = req.headers["authorization"] ?? "";
-  const decoded = jwt.verify(token, JWT_SECRET);
+  const decoded = jwt.verify(token, JWT_SECRET) as any;
   if (decoded) {
-    req.userId = decoded.userId;
+    (req as any) = decoded.userId;
     next();
   } else {
     res.status(403).json({
