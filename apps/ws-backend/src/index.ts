0d676aa33f92eb0fc7ad675671985ff753d42dc8
Sattarkhan37 <abdulsattarkhan973@gmail.com>
2026-04-17 19:59:53 +0530

    storing chats in DB

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,7 +1,7 @@
 import { WebSocketServer, WebSocket as WSWebSocket } from "ws";
 import jwt, { JwtPayload } from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
-import {prismaClient} from '@repo/db/client'
+import { prismaClient } from "@repo/db/client";
 const wss = new WebSocketServer({ port: 8081 });
 interface User {
   ws: WSWebSocket;
@@ -41,7 +41,7 @@ wss.on("connection", function connection(ws, request) {
     rooms: [],
     ws,
   });
-  ws.on("message", function message(data) {
+  ws.on("message", async function message(data) {
     const parsedData = JSON.parse(data as unknown as string);
     if (parsedData.type === "join_room") {
       const user = users.find((x) => x.ws == ws);
@@ -57,7 +57,13 @@ wss.on("connection", function connection(ws, request) {
     if (parsedData.type === "chat") {
       const roomId = parsedData.roomId;
       const message = parsedData.message;
-      await 
+      await prismaClient.chat.create({
+        data: {
+          roomId,
+          message,
+          userId,
+        },
+      });
       users.forEach((user) => {
         if (user.rooms.includes(roomId)) {
           user.ws.send(
