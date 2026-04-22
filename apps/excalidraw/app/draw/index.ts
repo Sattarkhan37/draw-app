91e03b5c9139de5b165c455b2bdc06ff7c613a5d
Sattarkhan37 <abdulsattarkhan973@gmail.com>
2026-04-22 21:26:39 +0530

    bug fix while creating rectangle and added circle

--- apps/excalidraw/app/draw/index.ts
+++ apps/excalidraw/app/draw/index.ts
@@ -1,6 +1,21 @@
+type shape =
+  | {
+      type: "rect";
+      x: number;
+      y: number;
+      width: number;
+      height: number;
+    }
+  | {
+      type: "circle";
+      centerX: number;
+      centerY: number;
+      radius: number;
+    };
 export function initDraw(canvas: HTMLCanvasElement) {
   const ctx = canvas.getContext("2d");
 
+  let existingShapes: shape[] = [];
   if (!ctx) {
     return;
   }
@@ -9,25 +24,57 @@ export function initDraw(canvas: HTMLCanvasElement) {
   let clicked = false;
   let startX = 0;
   let startY = 0;
+  const getMousePos = (e: MouseEvent) => {
+    const rect = canvas.getBoundingClientRect();
+    return {
+      x: e.clientX - rect.left,
+      y: e.clientY - rect.top,
+    };
+  };
   canvas.addEventListener("mousedown", (e) => {
     clicked = true;
-    startX = e.clientX;
-    startY = e.clientY;
+    const pos = getMousePos(e);
+    startX = pos.x;
+    startY = pos.y;
   });
   canvas.addEventListener("mouseup", (e) => {
     clicked = false;
-    console.log("up", e.clientX);
-    console.log("up", e.clientY);
+    const pos = getMousePos(e);
+    const width = pos.x - startX;
+    const height = pos.y - startY;
+    existingShapes.push({
+      type: "rect",
+      x: startX,
+      y: startY,
+      height,
+      width,
+    });
   });
   canvas.addEventListener("mousemove", (e) => {
     if (clicked) {
-      const width = e.clientX - startX;
-      const height = e.clientY - startY;
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      ctx.fillStyle = " rgba(0,0,0)";
-      ctx.fillRect(0, 0, canvas.width, canvas.height);
+      const pos = getMousePos(e);
+      const width = pos.x - startX;
+      const height = pos.y - startY;
+      clerCanvas(existingShapes, canvas, ctx);
       ctx.strokeStyle = "rgba(255,255,255)";
       ctx.strokeRect(startX, startY, width, height);
+    }
+  });
+}
+
+function clerCanvas(
+  existingShapes: shape[],
+  canvas: HTMLCanvasElement,
+  ctx: CanvasRenderingContext2D,
+) {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = " rgba(0,0,0)";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  existingShapes.map((shape) => {
+    if (shape.type === "rect") {
+      ctx.strokeStyle = "rgba(255,255,255)";
+      ctx.strokeRect(shape.x, shape.y, shape.width, shape.height);
     }
   });
 }
