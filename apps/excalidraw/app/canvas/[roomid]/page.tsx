87cc7f48cb98777f94737de3c5258d8ed5c91e20
Sattarkhan37 <abdulsattarkhan973@gmail.com>
2026-04-22 20:25:39 +0530

    created initDraw function to maintain clean code

--- apps/excalidraw/app/canvas/[roomid]/page.tsx
+++ apps/excalidraw/app/canvas/[roomid]/page.tsx
@@ -1,36 +1,12 @@
 "use client";
+import { initDraw } from "@/app/draw";
 import { useEffect, useRef } from "react";
 
 export default function Canvas() {
   const canvasref = useRef<HTMLCanvasElement>(null);
   useEffect(() => {
     if (canvasref.current) {
-      const canvas = canvasref.current;
-      const ctx = canvas.getContext("2d");
-      if (!ctx) {
-        return;
-      }
-      let clicked = false;
-      let startX = 0;
-      let startY = 0;
-      canvas.addEventListener("mousedown", (e) => {
-        clicked = true;
-        startX = e.clientX;
-        startY = e.clientY;
-      });
-      canvas.addEventListener("mouseup", (e) => {
-        clicked = false;
-        console.log("up", e.clientX);
-        console.log("up", e.clientY);
-      });
-      canvas.addEventListener("mousemove", (e) => {
-        if (clicked) {
-          const width = e.clientX - startX;
-          const height = e.clientY - startY;
-          ctx.clearRect(0, 0, canvas.width, canvas.height);
-          ctx.strokeRect(startX, startY, width, height);
-        }
-      });
+      initDraw(canvasref.current);
     }
   }, [canvasref]);
   return (
